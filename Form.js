312e33318af097c65e98ba6866a6d27ae7ffbd9c
Robert Gonzalez <robertgonzalez@utexas.edu>
2017-07-07 17:00:13 -0500

    cleaned up js code

--- Form.js
+++ Form.js
@@ -2,30 +2,29 @@ $(document).ready(function() {
     $('#buttonClick').click(function(e) {
       e.preventDefault();
       if ($('#pwd').val() != $('#pwdConfirm').val()) {
-  console.log($('#pwd').val())
-    console.log('Error - please reenter')}
-    else {
-      $(this).attr('disabled','disabled'); // Switch to default grey
-      $(this).html("Sending"); // Change text of button
-      $.ajax({
-        async: true,
-        type: 'POST',
-        url: "http://putsreq.com/pXCdPhkhgE0q3QjD0tjg",
-        data: $('#commentForm #userInfo').serialize(),
-        dataType: 'text',
-        crossDomain: true,
-        complete: function() {
-          $("#submitInformation").removeAttr('disabled'); // Change back text of button
-        },
-        error: function(request, errorType, errorMessage) {
-          $("#formStatus").text("Information entry failed. Error: " + errorType + " " + errorMessage);
-        },
-        success: function(response) {
-          $("#formStatus").text("Information sent successfully.");
-          $('#enterInformation').html("Enter Information");
+        console.log('Error - please reenter')}
+      else {
+        $(this).attr('disabled','disabled'); // Switch to default grey
+        $(this).html("Sending"); // Change text of button
+        $.ajax({
+          async: true,
+          type: 'POST',
+          url: "http://putsreq.com/pXCdPhkhgE0q3QjD0tjg",
+          data: $('#commentForm #userInfo').serialize(),
+          dataType: 'text',
+          crossDomain: true,
+          complete: function() {
+            $("#submitInformation").removeAttr('disabled'); // Change back text of button
+          },
+          error: function(request, errorType, errorMessage) {
+            $("#formStatus").text("Information entry failed. Error: " + errorType + " " + errorMessage);
+          },
+          success: function(response) {
+            $("#formStatus").text("Information sent successfully.");
+            $('#enterInformation').html("Enter Information");
+          }
+          });
         }
-        });
-      }
-    });
+      });
  });
 
